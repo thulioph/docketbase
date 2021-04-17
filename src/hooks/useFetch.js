370fb19d2f7456805e579ade9a5b643c3120c424
Thulio Philipe <thulioph@gmail.com>
2021-04-17 01:21:16 -0300

    feat: adds hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+
+const getApiUrl = () => {
+    if (process.env.NODE_ENV === 'development') {
+        return 'http://localhost:3000'
+    }
+
+    return 'http://localhost:3000' // replace it to your external API
+}
+
+const useFetch = () => {
+    const [loading, setLoading] = useState(true)
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true)
+
+            const response = await fetch(`${getApiUrl()}/api/hello`)
+            const result = await response.json()
+
+            setData(result.data)
+            setLoading(false)
+        }
+
+        fetchData()
+    }, [])
+
+    return { loading, data }
+}
+
+export default useFetch
